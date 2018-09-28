c0f61a4c48e63ae0ef97ed55d32a3aeed5073a6d
Matheus <matheusqs10000@hotmail.com.br>
2018-09-28 13:46:18 -0300

    Adicionando alteracoes web

---
+++ Mochilando/src/java/BeansGerenciaveis/Car.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BeansGerenciaveis;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Car {
+    Car(String marca, String tipo, int ano, String cor, int valor, boolean vende){
+        
+    }
+        
+}
